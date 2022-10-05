e993287a9d131755afb66b5a3e27165d714f34c1
Wilson-Chu <wilson1.chu@gmail.com>
2022-10-05 01:37:06 -0400

    updated UX so that sign in/out buttons show up correctly, and included Google One Tap

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ function App() {
     let userObject = jwt_decode(response.credential);
     console.log(userObject);
     setUser(userObject);
+
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event) {
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
   }
 
   useEffect(() => {
@@ -23,17 +30,21 @@ function App() {
       theme: "outline",
       size: "large",
     });
+
+    google.accounts.id.prompt(); // Google One Tap Dialogue
   }, []);
 
   // If we have no user: show the sign in button
   // If we have a user: show the log out button
-
   return (
     <div className="App">
       <div id="signInDiv"></div>
+      {Object.keys(user).length != 0 && ( // If user signed in
+        <button onClick={(e) => handleSignOut(e)}>Sign Out</button>
+      )}
       {user && (
         <div>
-          <img src={user.picture} alt="Profile" />
+          <img src={user.picture} />
           <h3>{user.name}</h3>
         </div>
       )}
