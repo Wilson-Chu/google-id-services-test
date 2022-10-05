0ff73be23f4252d4c9356159fbe8992b8c7c0722
Wilson-Chu <wilson1.chu@gmail.com>
2022-10-05 00:27:38 -0400

    added Google token for auth via .env file, works well

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  function handleCallbackResponse(response) {
+    console.log("JWT token: " + response.credential);
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: process.env.REACT_APP_GOOGLE_ID_TOKEN,
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div id="signInDiv"></div>
+    </div>
+  );
 }
 
 export default App;
